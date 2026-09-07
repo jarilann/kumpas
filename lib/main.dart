607ffa8514208786431b5443304ee1b677d8cd8a
Stephanie G. Sagun <stephanie.saguns@gmail.com>
2026-09-07 21:40:36 +0800

    name of the app changed to KumpasKonek

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,7 @@ class KumpasKonekApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      title: 'KumpasKonek',
       debugShowCheckedModeBanner: false,
       home: AuthWrapper(),
     );
